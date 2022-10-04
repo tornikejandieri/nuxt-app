794c8bb21d1378d95e3fa3803db92e2c99d86ebc
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-10-04 18:35:55 +0400

    layout

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -7,5 +7,6 @@ export default {
   validate(data) {
     return /^\d+$/.test(data.params.id);
   },
+  layout: "users",
 };
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,9 @@
 <template>
   <p>Please enter an Id and click the button</p>
 </template>
+
+<script>
+export default {
+  layout: "users",
+};
+</script>
